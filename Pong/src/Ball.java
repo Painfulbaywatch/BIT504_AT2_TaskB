a6d765c72ea277c03e2c810aef8b99d864c21bfb
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-08-20 21:29:24 +1200

    change ball colour to red

--- Pong/src/Ball.java
+++ Pong/src/Ball.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 
 public class Ball extends Sprite{
 	
-	private final static Color BALL_COLOUR = Color.black;
+	private final static Color BALL_COLOUR = Color.red;
 	private final static int BALL_WIDTH = 25;
 	private final static int BALL_HEIGHT = 25;
 	
